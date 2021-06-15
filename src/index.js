829a50544a356c3b457efae7f1dfd0eeb50982db
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-15 15:40:46 +0900

    basic actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0, action) => {
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  count--;
-  updateText();
-};
+// data를 저장할 store
+const countStore = createStore(countModifier);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// dispatch를 통해 상태 업데이트를 알림(매개변수는 action)
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
