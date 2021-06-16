a5ba7d64315f3a7a2e95225d167aa4cb699981b2
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-16 15:56:48 +0900

    mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
 // store로부터 state를 가져올 수 있어야 함.
-function Home(props) {
-  console.log(props);
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
-    console.log(text);
+    addToDo(text);
     setText("");
   }
   return (
@@ -20,7 +20,7 @@ function Home(props) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
@@ -29,4 +29,10 @@ function mapStateToProps(state, ownProps) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
