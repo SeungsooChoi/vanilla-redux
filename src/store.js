f52cf6689374e603ddc24f7c95d91345f87c4f15
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-17 15:26:44 +0900

    Feat : Add Redux Toolkit(createAction)

--- src/store.js
+++ src/store.js
@@ -1,29 +1,22 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
+const addToDo = createAction("ADD", (text) => {
   return {
-    type: ADD,
-    text,
-    id: Date.now(),
+    payload: {
+      text,
+      id: Date.now(),
+    },
   };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+});
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [...state, { text: action.text, id: action.id }];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [...state, { text: action.payload.text, id: action.payload.id }];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
