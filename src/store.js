2d2e87f2d916cf7a3566d7321f60b3cbadacc09d
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-17 15:44:54 +0900

    Feat : Add createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD", (text) => {
   return {
@@ -11,6 +11,7 @@ const addToDo = createAction("ADD", (text) => {
 });
 const deleteToDo = createAction("DELETE");
 
+/*
 const reducer = (state = [], action) => {
   switch (action.type) {
     case addToDo.type:
@@ -21,6 +22,28 @@ const reducer = (state = [], action) => {
       return state;
   }
 };
+*/
+
+// redux toolkit을 사용할때는 state를 mutate시켜도 된다?
+// 방식이 2가지가 있는데 Map Object 방식보다 builder callback방식을 권장함.
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload.text, id: action.payload.id });
+    })
+    .addCase(deleteToDo, (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload)
+    );
+
+  /*
+    // Map Object
+    [addToDo]: (state, action) => {
+      state.push({ text: action.payload.text, id: action.payload.id });
+    },
+    [deleteToDo]: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
+      */
+});
 
 const store = createStore(reducer);
 
